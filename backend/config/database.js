e2bb0c3d9ee1bda48c942a10cb1d03775ff8a280
Tomma <tommaso.barletta.g@gmail.com>
2025-04-09 19:35:28 +0200

    fix vari

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
+require('dotenv').config({ path: process.env.NODE_ENV === 'production' ? undefined : '../.env' }); // Modifica questa linea
+
 const { Sequelize } = require('sequelize');
+
+// Aggiungi controllo di sicurezza
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL non definito! Controlla le variabili d\'ambiente');
+}
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: process.env.NODE_ENV === 'production' ? {
     ssl: {
       require: true,
